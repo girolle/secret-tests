89ae956266f69be25b95225e2843fe276686ee4e
Alexandr Kulikov <kulik-alex@yandex-team.ru>
2021-10-25 15:25:04 +0300

    get_test

--- secret-tests/run.sh
+++ secret-tests/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function print_green(){
+	echo -e "\e[32m$@\e[0m"
+}
+function print_red(){
+	echo -e "\e[31m$@\e[0m"
+}
+
 function check(){
 	if [[ $? != 0 ]]
 		then
@@ -7,9 +14,6 @@ function check(){
 		exit 1
 	fi
 }
-
-node open-tests/run.js
-check
 
 node open-tests/http_get_test.js
 check
