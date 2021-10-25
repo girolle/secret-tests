587ba4a1d39bcf0fa8c4ee2ecd8ee8526bbbc4a4
Alexandr Kulikov <kulik-alex@yandex-team.ru>
2021-10-25 16:38:49 +0300

    get_test

--- secret-tests/http_get_test.js
+++ secret-tests/http_get_test.js
@@ -24,10 +24,11 @@ function get(reletiveURI) {
             res.on('data', (chunk) => { rawData += chunk; });
             res.on('end', () => {
                 resolve({ done: true, data: { res, rawData } });
+                console.log("\n\n\n\nin get ok\n\n\n\n");
             });
         }).on('error', error => {
             resolve({ done: false });
-            console.log("\n\n\n\nin get\n\n\n\n");
+            console.log("\n\n\n\nin get err\n\n\n\n");
         });
         req.end();
     });
